57294f549a034376188cc28cce883a62902c1b96
DengreSarthak <dengresathak2002@gmail.com>
2024-12-16 02:19:37 +0530

    New address for HodlCoinFactory

--- utils/addresses.ts
+++ utils/addresses.ts
@@ -1,7 +1,7 @@
 export const NFTAddress = '0xfBAc93dD05a880634232883f92b9e1d4891c9700'
 
 export const HodlCoinVaultFactories = {
-    534351: "0x5C3cEb66bb2Ef3De3D065f9C652Ac0fE7bC437b4"
+    534351: "0x3987E67823Acc8356bc947C3148f47a6D7F6c166"
 } as {
     [key: number]: `0x${string}`
 }
