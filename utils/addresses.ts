53274596335cdfc5dd2622b27c3c03ec12affa6f
DengreSarthak <dengresathak2002@gmail.com>
2024-12-23 23:03:16 +0530

    StyleButton change, address of factory contract, remove vaultName, abi of factory updated.

--- utils/addresses.ts
+++ utils/addresses.ts
@@ -1,7 +1,7 @@
 export const NFTAddress = '0xfBAc93dD05a880634232883f92b9e1d4891c9700'
 
 export const HodlCoinVaultFactories = {
-  534351: '0xd9E7848Ba881DABb8AF8C7b37fB681039B83DE50',
+  534351: '0xf8E20eCaAB1fc2a8868eECbcCd1E29b7995bAdC6',
 } as {
   [key: number]: `0x${string}`
 }
--- utils/props.ts
+++ utils/props.ts
@@ -1,6 +1,5 @@
 export type vaultsProps = {
   vaultAddress: `0x${string}`
-  vaultName: string
   coinName: string
   coinAddress: `0x${string}`
   coinSymbol: string
