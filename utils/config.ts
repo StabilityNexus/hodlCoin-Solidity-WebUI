9fd5f0053e13d3b9812d6362c994fa7ec9e8b6a8
DengreSarthak <dengresathak2002@gmail.com>
2025-01-31 23:48:26 +0530

    Seperated EthereumClassic and Milkomeda

--- utils/config.ts
+++ utils/config.ts
@@ -14,59 +14,11 @@ import {
   darkTheme,
 } from '@rainbow-me/rainbowkit'
 import { citreaTestnet } from '@/components/CitreaTestnet'
-
-export const ethereumClassic = {
-  id: 61, // Ethereum Classic chain ID
-  name: 'Ethereum Classic',
-  network: 'ethereumClassic',
-  nativeCurrency: {
-    name: 'Ethereum Classic',
-    symbol: 'ETC',
-    decimals: 18,
-  },
-  rpcUrls: {
-    default: {
-      http: ['https://etc.rivet.link'],
-    },
-    public: {
-      http: ['https://etc.rivet.link'],
-    },
-  },
-  blockExplorers: {
-    default: {
-      name: 'BlockScout',
-      url: 'https://blockscout.com/etc/mainnet',
-    },
-  },
-}
-
-export const milkomeda = {
-  id: 2001, // Milkomeda C1 (Cardano sidechain) chain ID
-  name: 'Milkomeda',
-  network: 'milkomeda',
-  nativeCurrency: {
-    name: 'MilkADA',
-    symbol: 'mADA',
-    decimals: 18,
-  },
-  rpcUrls: {
-    default: {
-      http: ['https://rpc-mainnet-cardano-evm.c1.milkomeda.com'],
-    },
-    public: {
-      http: ['https://rpc-mainnet-cardano-evm.c1.milkomeda.com'],
-    },
-  },
-  blockExplorers: {
-    default: {
-      name: 'Milkomeda Explorer',
-      url: 'https://explorer-mainnet-cardano-evm.c1.milkomeda.com',
-    },
-  },
-}
+import { ethereumClassic } from '@/components/EthereumClassic'
+import { milkomeda } from '@/components/Milkomeda'
 
 export const config = getDefaultConfig({
-  appName: 'clowder',
+  appName: 'hodlcoin',
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID ?? '',
   chains: [
     scrollSepolia,
