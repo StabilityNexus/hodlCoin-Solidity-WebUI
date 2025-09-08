e02d33f4955a29a01655988533e683694373cdfb
DengreSarthak <dengresarthak2002@gmail.com>
2025-09-08 22:58:46 +0530

    Made hodlcoin multichain

--- components/Vault/VaultInformation.tsx
+++ components/Vault/VaultInformation.tsx
@@ -6,6 +6,8 @@ import { Button } from '@/components/ui/button'
 const BLOCK_EXPLORERS: { [key: number]: string } = {
   1: 'https://etherscan.io',
   61: 'https://blockscout.com/etc/mainnet',
+  137: 'https://polygonscan.com',
+  8453: 'https://basescan.org',
   2001: 'https://explorer-mainnet-cardano-evm.c1.milkomeda.com',
   534351: 'https://sepolia.scrollscan.com',
   5115: 'https://explorer.testnet.citrea.xyz',
